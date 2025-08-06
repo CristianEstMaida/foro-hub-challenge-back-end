351259aa3e1f65ca1d9db1cd04b4ce222afbb666
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2025-08-06 14:11:58 -0300

    Add files via upload

--- src/main/java/com/foro/hub/foro_hub_api/controller/TopicsController.java
+++ src/main/java/com/foro/hub/foro_hub_api/controller/TopicsController.java
@@ -3,6 +3,7 @@ package com.foro.hub.foro_hub_api.controller;
 import com.foro.hub.foro_hub_api.domain.course.CourseRepository;
 import com.foro.hub.foro_hub_api.domain.topic.*;
 import com.foro.hub.foro_hub_api.domain.user.UserRepository;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +12,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import static org.springframework.data.domain.Sort.Direction.DESC;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Optional;
+
 @RestController
 @RequestMapping("/topics")
+@SecurityRequirement(name = "bearer-key")
 public class TopicsController {
+
+
+    private final TopicService topicService;
 
     @Autowired
     private TopicRepository topicRepository;
@@ -32,71 +35,47 @@ public class TopicsController {
     @Autowired
     private UserRepository userRepository;
 
+    public TopicsController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
     @GetMapping
     @CacheEvict(value = "topicsList")
-    public Page<TopicDTO> list(@RequestParam(required = false) String courseName,
+    public ResponseEntity<Page<TopicDetailsDTO>> list(@RequestParam(required = false) String courseName,
                                @RequestParam(required = false) Integer year,
                                @PageableDefault(sort = "creationDate", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable pagination) {
-
-        Page<Topic> topics;
-        if (courseName != null && year != null) {
-            topics = topicRepository.findByCourseNameAndYear(courseName, year, pagination);
-
-        }
-        else if (courseName != null) {
-            topics = topicRepository.findByCourseName(courseName, pagination);
-        } else {
-            topics = topicRepository.findAll(pagination);
-        }
-        System.out.println(topics.getTotalElements());
-        return TopicDTO.convert(topics);
+        var response = topicService.list(courseName, year, pagination);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping
     @Transactional
     @CacheEvict(value = "topicsList", allEntries = true)
-    public ResponseEntity<TopicDTO> register(@RequestBody @Valid TopicForm form, UriComponentsBuilder uriBuilder) {
-        if (topicRepository.existsByTitleAndMessage(form.title(), form.message())){
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .build();
-        }
+    public ResponseEntity<TopicDetailsDTO> register(@RequestBody @Valid TopicFormDTO form, UriComponentsBuilder uriBuilder) {
+        TopicDetailsDTO topicDTO = topicService.register(form);
 
-        Topic topic = form.convert(courseRepository, userRepository);
-        topicRepository.save(topic);
-
-        URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
-        return ResponseEntity.created(uri).body(new TopicDTO(topic));
+        URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topicDTO.id()).toUri();
+        return ResponseEntity.created(uri).body(topicDTO);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TopicDetailsDTO> detail(@PathVariable Long id) {
-        Optional<Topic> topic = topicRepository.findById(id);
-        if (topic.isPresent()) {
-            return ResponseEntity.ok(new TopicDetailsDTO(topic.get()));
-        }
-        return ResponseEntity.notFound().build();
+        TopicDetailsDTO dto = topicService.getDetails(id); // Maneja lógica y errores
+        return ResponseEntity.ok(dto);
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<TopicDTO> update(@PathVariable Long id, @RequestBody @Valid TopicUpdateForm form) {
-        Optional<Topic> optional = topicRepository.findById(id);
-        if (optional.isPresent()) {
-            Topic topic = form.update(id, topicRepository, courseRepository);
-            return ResponseEntity.ok(new TopicDTO(topic));
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<TopicDetailsDTO> update(@PathVariable Long id, @RequestBody @Valid TopicUpdateFormDTO form) {
+        var response = topicService.update(form);
+        return ResponseEntity.ok(new TopicDetailsDTO(response));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     @CacheEvict(value = "topicsList", allEntries = true)
-    public ResponseEntity<?> delete(@PathVariable Long id) {
-        Optional<Topic> topic = topicRepository.findById(id);
-        if (topic.isPresent()) {
-            topicRepository.deleteById(id);
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<?> delete(@PathVariable TopicCancelamientoDTO datos) {
+        topicService.delete(datos);
+        return ResponseEntity.noContent().build();
     }
 }
