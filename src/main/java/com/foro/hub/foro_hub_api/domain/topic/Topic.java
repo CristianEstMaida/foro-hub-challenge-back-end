6b0af689fe263ab01b3229c417ab4fc8e8f62609
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2025-08-06 14:20:18 -0300

    Add files via upload

--- src/main/java/com/foro/hub/foro_hub_api/domain/topic/Topic.java
+++ src/main/java/com/foro/hub/foro_hub_api/domain/topic/Topic.java
@@ -2,8 +2,10 @@ package com.foro.hub.foro_hub_api.domain.topic;
 
 import com.foro.hub.foro_hub_api.domain.answer.Answer;
 import com.foro.hub.foro_hub_api.domain.course.Course;
+import com.foro.hub.foro_hub_api.domain.course.CourseRepository;
 import com.foro.hub.foro_hub_api.domain.user.User;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,9 +26,10 @@ public class Topic {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "titulo")
+
+    @Column(name = "titulo", unique = true)
     private String title;
-    @Column(name = "mensaje")
+    @Column(name = "mensaje", unique = true)
     private String message;
     @Column(name = "fecha_creacion")
     private LocalDateTime creationDate = LocalDateTime.now();
@@ -46,5 +49,14 @@ public class Topic {
         this.message = message;
         this.course = course;
         this.user = user;
+    }
+
+    public void update(@Valid TopicUpdateFormDTO form, CourseRepository courseRepository) {
+        this.title = form.title();
+        this.message = form.message();
+        this.course = courseRepository.findByName(form.courseName())
+                .orElseThrow(() -> new RuntimeException("Curso no encontrado"));
+
+
     }
 }
